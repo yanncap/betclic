b65796ac64a560d3d8745317717be03216499074
yanncap <gitrhuiDean296>
2017-02-07 22:19:18 +0100

    Ajouts :            -image ligue1.png en tant que banniere
    Modification/évolution
               -paris.html : ajout d'un surecran récapitulatif paris et connection
               -paris.js: prise en compte de la classe modal2 css(provisoire) et fonctionnement de la page paris.html
               -theme_paris.css: prise en compte des nouvelle classe et identifiants html.

--- js/paris.js
+++ js/paris.js
@@ -1,13 +1,18 @@
 (function($){
 
     $('.modal').hide();
+    $('.modal2').hide();
 
     $('#validerParis').on('click', function () {
         $('.shadow, .modal').show();
     });
 
     $('.shadow').on('click', function () {
-        $('.shadow, .modal').hide();
+        $('.shadow, .modal, .modal2').hide();
+    });
+
+    $('.nul').on('click', function () {
+        $('.shadow, .modal2').show();
     });
 
 })(jQuery);
