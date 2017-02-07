0243ed31b166d78448953541a6fbf76381426fcd
idschiff <idschiff@gmail.com>
2017-02-07 23:51:40 +0100

    Ajout des boutons Valider et Annuler les modif sur la page de Mon compte

--- js/compte.js
+++ js/compte.js
@@ -8,18 +8,28 @@
         value = $('#experation').attr('value');
         if(value == ""){
 
-            $('.statut').css("display","inline-block");
+            $('.aletre_carte').css("display","inline-block");
         }
 
         $('#modif-button').on('click',function () {
 
                 $('.statut').attr("disabled",false);
+                $('#valid-button,#annuler-button').css("display","inline-block");
 
         })
 
         $('#valid-button').on('click',function () {
 
             $('.statut').attr("disabled",true);
+            $(this).css("display","none");
+            $('#annuler-button').css("display","none");
+
+        })
+        $('#annuler-button').on('click',function () {
+
+            $('.statut').attr("disabled",true);
+            $(this).css("display","none");
+            $('#valid-button').css("display","none");
 
         })
     });
