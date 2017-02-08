f2342aac2f3d60578c05455c015205a1df93655c
idschiff <idschiff@gmail.com>
2017-02-08 16:54:49 +0100

    MAJ compte

--- js/compte.js
+++ js/compte.js
@@ -5,29 +5,35 @@
 (function ($) {
     $(function(){
 
-      validerChamps();
+        $('.aletre_carte,#valid-button,#annuler-button').hide();
 
+      validerChamps();
 
         $('#modif-button').on('click',function () {
 
                 $('.statut').attr("disabled",false);
-                $('#valid-button,#annuler-button').css("display","inline-block");
+               // $('#valid-button,#annuler-button').css("display","inline-block");
+            $('#valid-button,#annuler-button').show();
 
         })
 
         $('#valid-button').on('click',function () {
 
             $('.statut').attr("disabled",true);
-            $(this).css("display","none");
-            $('#annuler-button').css("display","none");
+            $(this).hide();
+            $('#annuler-button').hide();
+           /* $(this).css("display","none");
+            $('#annuler-button').css("display","none");*/
             validerChamps();
 
         })
         $('#annuler-button').on('click',function () {
 
             $('.statut').attr("disabled",true);
-            $(this).css("display","none");
-            $('#valid-button').css("display","none");
+           /* $(this).css("display","none");
+            $('#valid-button').css("display","none");*/
+            $(this).hide();
+            $('#valid-button').hide();
             validerChamps();
 
         })
@@ -35,10 +41,12 @@
 
     function validerChamps(){
         if ($('#nomPorteur').val()==""||$('#numCarte').val()==""||$('#codeSecurite').val()==""||$('#experation').val()==""){
-            $('.aletre_carte').css("display","inline-block");
+          //  $('.aletre_carte').css("display","inline-block");
+            $('.aletre_carte').show();
         }
         else{
-            $('.aletre_carte').css("display","none");
+         //   $('.aletre_carte').css("display","none");
+            $('.aletre_carte').hide();
         }
     }
 
