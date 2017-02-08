73acffe0d7b90b4375f0a4b90ec31607cdfa2387
formation <yann.cap@live.com>
2017-02-08 17:02:33 +0100

    MAJ :               -paris.html : refactoring               -paris.js : refactoring               -them_paris.css : refactoring

--- js/paris.js
+++ js/paris.js
@@ -1,45 +1,46 @@
 (function($){
 
-    $('.modal').hide();
-    $('.modal2').hide();
-    $('#encadreConnection').hide();
-    $('#miseInput').val("");
+    function init(){
+        $('#miseInput').val("");
+    }
 
-    $('#validerParis').on('click', function () {
-        $('.shadow, .modal').show();
-    });
+    $('#validerParis').on('click', function (){displayParisSelected()});
 
     $('.shadow').on('click', function () {
-        $('.shadow, .modal, .modal2').hide();
+        $('.shadow, .modal').hide();
         $('#miseInput').val("");
     });
 
     $('.vic a').on('click', function(){
         console.log("valeur de la cote : " + $(this).html());
         $('#cote').html($(this).html());
-        var tempURL = $(this).parent().parent().find(".infoParis p:nth-child(2)");
-        console.log("valeur du match : " + $(tempURL).html()/* $(' .infoParis p:nth-child(2)').html()*/);
-        $('#selectedMatch').html($(tempURL).html());
-        $('.shadow, .modal2').show();
+        var findMatchDescription = $(this).parents('.ligneParis').find(".infoParis p:nth-child(2)");
+        console.log("valeur du match : " + $(findMatchDescription).html()/* $(' .infoParis p:nth-child(2)').html()*/);
+        $('#selectedMatch').html($(findMatchDescription).html());
+        $('.shadow, .modal').show();
     });
 
     $('#miseInput').on('input', function(){calculGain()})
 
-    var login = "";
-    var password = "";
 
     $('#validerParis').on('click', function () {
         if(!userCoo){
-            $('#parisValide').addClass('colorRed').html('Vous devez vous connecter pour parier !')
+            $('#parisValide').addClass('colorInformation').html('Vous devez vous connecter pour parier !')
         }
         else{
-            $('#parisValide').addClass('colorRed').html('Votre paris a bien été pris en compte')
+            $('#parisValide').addClass('colorInformation').html('Votre paris a bien été pris en compte')
         }
     })
 
 })(jQuery);
 
+
+
 function calculGain(){
     var gains = Number($('#cote').html()) * Number($('#miseInput').val());
     $('#gainPoentiels').html(gains.toFixed(2));
+}
+
+function displayParisSelected(){
+    $('.shadow, .modal').show();
 }
