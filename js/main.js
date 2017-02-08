cc0518a6f3349b81ea6ee83764f7332f4949372c
formation <teo.dinel@gmail.com>
2017-02-08 09:22:32 +0100

    maj index.html V5 (caroussel version)

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@
  * Created by Gally on 06/02/2017.
  */
 var boolDico = false;
+var tabLien = ["foot1.jpg","foot2.jpg","foot3.jpg","foot4.jpg","tennis.jpg"];
+var tabTitre1 = ["Football, Ligue 1","Football, Ligue 2","Football, Ligue 1","Football, Ligue 1","Tennis"];
+var tabTitre2 = ["Paris SG - Lille","Strasbourg - Lyon", "Marseille - Barcelone", "Real M. - St Etienne","Nadal - Federer"];
+var interval;
+var target = 1;
+var NbrItemCarou = 5;
 
 $(function(){
     $(".menuMBouton").on("click", function(){lateral()});
@@ -10,11 +16,16 @@ $(function(){
 
     $("#okS").on("click", function(){fausseConnection(1)});
     $("#okM").on("click", function(){fausseConnection(2)});
+    initCarou();
+    interval = setInterval(travelingCarou,3000);
+    $(".caroussel").hover(stopCarou , playCarou);
 });
 
 function lateral(){
     $(".shadow").show();
+    $(".shadow").css('z-index', 1);
     $(".menuLateral").show();
+    $(".menuLateral").css('z-index', 2);
 }
 
 function lateralFin(){
@@ -49,7 +60,36 @@ function fausseConnection(choix){
     }
 }
 
+function initCarou(){
+    $(".fondCarou").css("width", NbrItemCarou*700+"px");
+    for (var i=0 ; i<NbrItemCarou ; i++){
+        $(".fondCarou").append("<div class='itemCarou' id='itemCarou"+i+"'><img src='img/"+ tabLien[i] +"'></div>");
+        $('#itemCarou'+i).append("<a href='html/paris.html'><div class='lienCarou'><h6 class='titreItem1'>"+ tabTitre1[i] +"</h6><h5 class='titreItem2'>"+ tabTitre2[i] +"</h5></div></a>");
+    }
+}
+
+function travelingCarou(){
+    if(target < NbrItemCarou){
+        $(".fondCarou").animate({left: "-=" + 700 + "px"}, 500);
+        target++;
+    }else{
+        reinitCarou();
+    }
+}
+
+function reinitCarou(){
+    $(".fondCarou").animate({left: "0px"}, 800);
+    target = 1;
+}
 /*function sizing(){
     var taillePage = $("body").innerWidth();
     console.log(taillePage);
 }*/
+
+function stopCarou(){
+    clearInterval(interval);
+}
+
+function playCarou(){
+    interval = setInterval(travelingCarou,3000);
+}
