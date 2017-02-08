604738c5a11f6be5ce251c9a1f7af1246ecfdd3c
formation <yann.cap@live.com>
2017-02-08 12:10:31 +0100

    MAJ :               -paris.html               -paris.js               -them_paris.css

--- js/paris.js
+++ js/paris.js
@@ -2,6 +2,8 @@
 
     $('.modal').hide();
     $('.modal2').hide();
+    $('#encadreConnection').hide();
+    $('#miseInput').val("");
 
     $('#validerParis').on('click', function () {
         $('.shadow, .modal').show();
@@ -9,11 +11,35 @@
 
     $('.shadow').on('click', function () {
         $('.shadow, .modal, .modal2').hide();
+        $('#miseInput').val("");
     });
 
-    $('.nul').on('click', function () {
+    $('.vic a').on('click', function(){
+        console.log("valeur de la cote : " + $(this).html());
+        $('#cote').html($(this).html());
+        var tempURL = $(this).parent().parent().find(".infoParis p:nth-child(2)");
+        console.log("valeur du match : " + $(tempURL).html()/* $(' .infoParis p:nth-child(2)').html()*/);
+        $('#selectedMatch').html($(tempURL).html());
         $('.shadow, .modal2').show();
     });
 
+    $('#miseInput').on('input', function(){calculGain()})
+
+    var login = "";
+    var password = "";
+
+    $('#validerParis').on('click', function () {
+        if(!userCoo){
+            $('#parisValide').addClass('colorRed').html('Vous devez vous connecter pour parier !')
+        }
+        else{
+            $('#parisValide').addClass('colorRed').html('Votre paris a bien été pris en compte')
+        }
+    })
+
 })(jQuery);
 
+function calculGain(){
+    var gains = Number($('#cote').html()) * Number($('#miseInput').val());
+    $('#gainPoentiels').html(gains.toFixed(2));
+}
